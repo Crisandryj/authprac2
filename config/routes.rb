a7d48c6bf0e766261948c2d722767b45c7d63bfc
crisandryj <crisandryj@gmail.com>
2022-09-28 17:04:40 -0400

    created confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'confirmations/new'
   get 'users/new'
   get 'staticpages/Home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
 end
