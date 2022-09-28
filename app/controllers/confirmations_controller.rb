76d8c40d0e91ff0cb9bb556ad8d7f29948561d2f
crisandryj <crisandryj@gmail.com>
2022-09-28 17:33:13 -0400

    complete confirmations controller methods

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,12 +5,24 @@ class ConfirmationsController < ApplicationController
 
   def create
     @user = User.new(Email: params[:user][:email].downcase)
+
+    if @user.present? && user.unconfirmed?
+      redirect_to root_path, notice: "Check your email for confirmation instructions."
+    else
+      redirect_to new_confirmation_path, alert: "We could not find a user with that email or that email has already been confirmed."
+    end
   end
 
-  if @user.present? && user.unconfirmed?
-    redirect_to root_path, notice: "Check your email for confirmation instructions."
-  else
-    redirect_to new_confirmation_path, alert: "We could not find a user with that email or that email has already been confirmed."
+  def edit
+    @user.find_signed(params[:confirmation_token], purpose: confirm_email)
+
+    if @user.present?
+      @user.confirm!
+      redirect_to root_path, notice: "your account has been confirmed"
+    else
+      redirect_to new_confirmation_path, alert: "Invalid token."
+    end
+
   end
-  end
+
 end
