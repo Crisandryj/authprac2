780c7bbef4fbb0b69f27ae478f297c2c4b3ee791
crisandryj <crisandryj@gmail.com>
2022-09-28 17:14:31 -0400

    added create method to confirmations

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,16 @@
 class ConfirmationsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(Email: params[:user][:email].downcase)
+  end
+
+  if @user.present? && user.unconfirmed?
+    redirect_to root_path, notice: "Check your email for confirmation instructions."
+  else
+    redirect_to new_confirmation_path, alert: "We could not find a user with that email or that email has already been confirmed."
+  end
   end
 end
