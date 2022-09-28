929e393f41ca4bcbea224cf42ef365850988cd61
crisandryj <crisandryj@gmail.com>
2022-09-28 19:03:22 -0400

    created concern for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+
   CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+  ...
+  MAILER_FROM_EMAIL = "no-reply@example.com"
+  ...
 
   has_secure_password
 
@@ -23,7 +27,12 @@ class User < ApplicationRecord
  def unconfirmed?
    !confirmed?
  end
- 
+
+ def send_confirmation_email!
+   confirmation_token = generate_confirmation_token
+   UserMailer.confirmation(self, confirmation_token).deliver_now
+ end
+
  private
 
  def downcase_email
