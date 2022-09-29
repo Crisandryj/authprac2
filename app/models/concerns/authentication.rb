3c43ba68edc326ae38fd5b953ad6552b24750c4d
crisandryj <crisandryj@gmail.com>
2022-09-29 13:09:15 -0400

    added current user method

--- app/models/concerns/authentication.rb
+++ app/models/concerns/authentication.rb
@@ -1,12 +1,14 @@
 module Authentication
   extend ActiveSupport::Concern
-  attribute :current_user
-  
+
   included do
     before_action :current_user
+    before_action :authenticate
     helper_method :current_user
     helper_method :user_signed_in?
   end
+
+
 
   def login(user)
     reset_session
@@ -22,13 +24,21 @@ module Authentication
   end
 
   def current_user
-   # Current.user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-   @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+   Current.user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
 
   end
 
   def user_signed_in?
-    @current_user.present?
+    Current.user.present?
   end
 
+  private
+    def authenticate
+      if authenticated_user = User.find_by(id: cookies.encrypted[:user_id])
+        Current.user = authenticated_user
+      else
+        redirect_to new_session_url
+      end
+    end
+    
 end
